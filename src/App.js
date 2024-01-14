5eb01f03580eb429566c0b489978240582f84f0f
srivasanth23 <jammulasrivasanth@gmail.com>
2024-01-14 13:48:21 +0530

    changed by srivasanth

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Myself Srivasanth
+          Myself h1
         </p>
         <h1>Hello Viswa Teja</h1>
       </header>
